b4e858e919c00be0439090dc2dc6b311d5e16278
Kevin Eifinger <k.eifinger@googlemail.com>
2020-12-07 14:22:35 +0100

    fix: Trigger release

--- fritz_switch_profiles/fritz_switch_profiles.py
+++ fritz_switch_profiles/fritz_switch_profiles.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python3
 
-# Copyright 2018 Florian Pigorsch & Contributors. All rights reserved.
+# Copyright 2020 Florian Pigorsch, Kevin Eifinger & Contributors. All rights reserved.
 #
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
