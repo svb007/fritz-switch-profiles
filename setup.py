5b4890a265029cf4b6612ab621f916cee64576a0
Kevin Eifinger <k.eifinger@googlemail.com>
2020-12-07 11:29:23 +0100

    chore: Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fritz-switch-profiles",
-    version="1.1.1",
+    version="1.1.2",
     author="Kevin Eifinger",
     author_email="k.eifinger@googlemail.com",
     description="A (Python) script to remotely set device profiles of an AVM Fritz!Box",
